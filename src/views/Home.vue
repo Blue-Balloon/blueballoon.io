2be4706957da279b6599746f5669de750387529a
unknown <darkslave@live.com>
2021-01-07 20:06:21 -0500

    contact section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -154,19 +154,19 @@
             clients
           </div> -->
           <div class="row">
-            <div class="col q-pa-lg">
+            <div class="col q-pa-md">
               <div
                 class="bg-grey-1 q-pa-md shadow-10"
                 style="border-radius: 15px;"
               >
                 <q-img
                   :src="require('@/assets/brewthers.png')"
-                  style="border-radius: 15px;"
+                  style="border-radius: 15px; position: relative; z-index: 0"
                 />
               </div>
               <div
                 class="bg-grey-1 q-pa-sm shadow-10"
-                style="border-radius: 15px; width: 25%; float: right; margin-right: 75px; margin-top: -150px;"
+                style="border-radius: 15px; width: 25%; float: right; margin-right: 75px; margin-top: -150px; z-index: 100; position: relative;"
               >
                 <q-img
                   :src="require('@/assets/brewthers_mobile.png')"
@@ -174,7 +174,92 @@
                 />
               </div>
             </div>
-            <div class="col">2</div>
+            <div class="col q-pa-md">
+              <div
+                class="text-h4 blueballoon-font weight-700 text-grey-9 q-mt-xl q-mb-md q-px-xl"
+              >
+                Brewthers
+              </div>
+              <div
+                class="text-body2 blueballoon-font weight-400 text-grey-7 q-mb-lg q-px-xl"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum diam dolor, interdum in sollicitudin lobortis,
+                pretium vel enim. Aliquam erat volutpat. In eget ante in nisl
+                blandit iaculis. Duis metus libero, tincidunt quis velit vel,
+                viverra iaculis tortor.
+              </div>
+              <div class="q-px-xl">
+                <q-btn
+                  rounded
+                  class="blueballoon-font text-bold gradient-two text-white"
+                  label="Visitar"
+                  no-caps
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col desktop-only"></div>
+      </div>
+    </section>
+    <section class="contact-section bg-grey-1">
+      <div class="row">
+        <div class="col desktop-only"></div>
+        <div class="col-lg-7">
+          <div class="row q-px-md">
+            <div class="text-h4 blueballoon-font weight-600 text-grey-9">
+              Escribenos:
+            </div>
+          </div>
+          <div class="row">
+            <div class="col q-pa-md">
+              <q-input
+                label="Nombre"
+                rounded
+                standout="bg-primary text-white"
+                class="blueballoon-font"
+              />
+            </div>
+            <div class="col q-pa-md">
+              <q-input
+                label="Telefono"
+                rounded
+                standout="bg-primary text-white"
+                class="blueballoon-font"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col q-pa-md">
+              <q-input
+                label="Correo"
+                rounded
+                standout="bg-primary text-white"
+                class="blueballoon-font"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col q-pa-md">
+              <q-input
+                label="Mensaje"
+                type="textarea"
+                rows="7"
+                rounded
+                standout="bg-primary text-white"
+                class="blueballoon-font"
+              />
+            </div>
+          </div>
+          <div class="row q-px-md">
+            <q-space />
+            <q-btn
+              rounded
+              class="blueballoon-font text-bold gradient-two text-white"
+              label="Enviar"
+              no-caps
+            />
           </div>
         </div>
         <div class="col desktop-only"></div>
@@ -240,5 +325,8 @@
 }
 .portfolio-section {
   padding: 50px 0;
+}
+.contact-section {
+  padding: 40px 0;
 }
 </style>
